113ca84ea6bf7d2d1e9ba11dd1bb9c9a0eded1f2
sophieF1 <sophie.field98@gmail.com>
2018-01-24 16:58:14 +0000

    moved the scraper out into its own service

--- routes/departures.js
+++ routes/departures.js
@@ -1,8 +1,10 @@
 var express = require('express');
 var router = express.Router();
+var scrapeItService = require('../services/scraperService');
 
 router.get('/', function(req, res) {
     res.render('pages/departures', {airportCode: req.query.airportCode});
+    scrapeItService.scrapeIt();
 });
 
 module.exports = router;
